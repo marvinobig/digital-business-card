1bdc87f42690f00833b4defd8d54ccd526bc4cdb
marvin obig <obig.marv@gmail.com>
2022-03-08 20:03:37 +0000

    added socials section component

---
+++ src/components/Socials.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Socials() {
+    return (
+        <footer className="social-links">
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-dev"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+        </footer>
+    )
+}
