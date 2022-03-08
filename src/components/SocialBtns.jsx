283bab9d5146100d4295f61c7bb390d046f336fd
marvin obig <obig.marv@gmail.com>
2022-03-08 20:15:58 +0000

    used correct syntax for css classes

--- src/components/SocialBtns.jsx
+++ src/components/SocialBtns.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function SocialBtn() {
     return (
         <section className="social-btn">
-            <a href="#"><button><i class="fa fa-envelope"></i> Email</button></a>
-            <a href="#"><button id="linkedin"><i class="fab fa-linkedin"></i> Linkedin</button></a>
+            <a href="#"><button><i className="fa fa-envelope"></i> Email</button></a>
+            <a href="#"><button id="linkedin"><i className="fab fa-linkedin"></i> Linkedin</button></a>
         </section>
     )
 }
