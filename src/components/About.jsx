033fdf338ff78ef3df90745466e0a9bad454ad63
marvin obig <obig.marv@gmail.com>
2022-03-08 20:02:54 +0000

    added about section component

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <section className="about">
+            <h2>About</h2>
+            <p>I am a frontend developer.  After my first experience with coding, I realised that this is something I enjoyed a lot, being able to have a mixture of 
+                creativity and logic and turning an idea into a product/service.</p>
+        </section>
+    )
+}
