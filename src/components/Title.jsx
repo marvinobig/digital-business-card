06e409beb27633ecbbf89e841ef303726045837f
marvin obig <obig.marv@gmail.com>
2022-03-08 20:03:21 +0000

    added title section component

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Title() {
+    return (
+        <section className="title">
+            <h1>Marvin Obig</h1>
+            <p>Frontend Developer</p>
+            <p id="email">mobig.web@gmail.com</p>
+        </section>
+    )
+}
