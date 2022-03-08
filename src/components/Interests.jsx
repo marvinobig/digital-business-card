9d33ff450fd5c1835110098a49acb56dc31db6e7
marvin obig <obig.marv@gmail.com>
2022-03-08 20:03:09 +0000

    added interests  section component

---
+++ src/components/Interests.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Interests() {
+    return (
+        <section className="interests">
+            <h2>Interests</h2>
+            <p>Fitness, Powerlifting, The MCU (Marvel Cinematic Universe),
+                Cyberpunk games, Action Movies And TV Shows.
+            </p>
+        </section>
+    )
+}
