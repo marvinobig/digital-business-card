704a030baf710d665ad7a462c30855366f3218fb
marvin obig <obig.marv@gmail.com>
2022-03-08 20:18:59 +0000

    corrected linkedin button text

--- src/components/SocialBtns.jsx
+++ src/components/SocialBtns.jsx
@@ -4,7 +4,7 @@ export default function SocialBtn() {
     return (
         <section className="social-btn">
             <a href="#"><button><i className="fa fa-envelope"></i> Email</button></a>
-            <a href="#"><button id="linkedin"><i className="fab fa-linkedin"></i> Linkedin</button></a>
+            <a href="#"><button id="linkedin"><i className="fab fa-linkedin"></i> LinkedIn</button></a>
         </section>
     )
 }
