f03f36137a6c8d16b0eed602a3844ac6e4d8bdfb
marvin obig <obig.marv@gmail.com>
2022-03-08 20:03:56 +0000

    added social media buttons component

---
+++ src/components/SocialBtns.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function SocialBtn() {
+    return (
+        <section className="social-btn">
+            <a href="#"><button><i class="fa fa-envelope"></i> Email</button></a>
+            <a href="#"><button id="linkedin"><i class="fab fa-linkedin"></i> Linkedin</button></a>
+        </section>
+    )
+}
