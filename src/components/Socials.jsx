dd4295990874473fc62b3e46dbceb6deae3b66bc
marvin obig <obig.marv@gmail.com>
2022-03-08 20:16:03 +0000

    used correct syntax for css classes

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function Socials() {
     return (
         <footer className="social-links">
-            <a href="#"><i class="fab fa-instagram"></i></a>
-            <a href="#"><i class="fab fa-dev"></i></a>
-            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i className="fab fa-instagram"></i></a>
+            <a href="#"><i className="fab fa-dev"></i></a>
+            <a href="#"><i className="fab fa-twitter"></i></a>
         </footer>
     )
 }
